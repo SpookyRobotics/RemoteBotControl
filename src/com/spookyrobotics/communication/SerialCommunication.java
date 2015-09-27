fdf02b0600a6d7dc48d69f33cea79d75c3bd9fe8
jsjrobotics <jsjrobotics@gmail.com>
2015-09-27 14:14:42 -0400

    Updated serial communication transmit bufer

--- src/com/spookyrobotics/communication/SerialCommunication.java
+++ src/com/spookyrobotics/communication/SerialCommunication.java
@@ -8,8 +8,10 @@ import gnu.io.SerialPort;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Enumeration;
 
 // http://rxtx.qbang.org/wiki/index.php/Two_way_communcation_with_the_serial_port
 public class SerialCommunication {
@@ -20,19 +22,26 @@ public class SerialCommunication {
    }
    private final String portName;
 
-   void connect() throws Exception
-   {
+   ArrayList<CommPort> getAvailablePorts(){
+       ArrayList<CommPort> result = new ArrayList<>();
+       Enumeration ports = CommPortIdentifier.getPortIdentifiers();
+       while(ports.hasMoreElements()){
+           Object port = ports.nextElement();
+           if(port instanceof CommPort){
+               result.add((CommPort) port);
+           }
+       }
+       return result;
+   }
+   void connect() throws Exception {
       CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
-      if ( portIdentifier.isCurrentlyOwned() )
-      {
+      if ( portIdentifier.isCurrentlyOwned() ) {
          System.out.println("Error: Port is currently in use");
       }
-      else
-      {
+      else {
          CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
 
-         if ( commPort instanceof SerialPort )
-         {
+         if ( commPort instanceof SerialPort ) {
             SerialPort serialPort = (SerialPort) commPort;
             serialPort.setSerialPortParams(9600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
 
@@ -43,8 +52,7 @@ public class SerialCommunication {
             (new Thread(new SerialWriter(out))).start();
 
          }
-         else
-         {
+         else {
             System.out.println("Error: Only serial ports are handled by this example.");
          }
       }
@@ -60,19 +68,15 @@ public class SerialCommunication {
          this.in = in;
       }
 
-      public void run ()
-      {
+      public void run () {
          byte[] buffer = new byte[1024];
          int len = -1;
-         try
-         {
-            while ( ( len = this.in.read(buffer)) > -1 )
-            {
+         try {
+            while ( ( len = this.in.read(buffer)) > -1 ) {
                System.out.print(new String(buffer,0,len));
             }
          }
-         catch ( IOException e )
-         {
+         catch ( IOException e ) {
             e.printStackTrace();
          }
       }
@@ -82,36 +86,44 @@ public class SerialCommunication {
    public static class SerialWriter implements Runnable
    {
       OutputStream out;
-       private ArrayList<Byte> transmitBuffer;
-
-       public SerialWriter ( OutputStream out )
-      {
+      private final ArrayList<Byte> transmitBuffer;
+      public SerialWriter ( OutputStream out ) {
          this.out = out;
+         transmitBuffer = new ArrayList<>();
       }
 
-      public void run ()
-      {
-         try
-         {
-            int c = 0;
-            while ( ( c = System.in.read()) > -1 )
-            {
-               this.out.write(c);
+      public void run () {
+         try {
+            int c = -1;
+            while ( transmitBuffer.size() > 0 || System.in.available() > -1 ) {
+               while(System.in.available() > -1){
+                  c = System.in.read();
+                  this.out.write(c);
+               }
+               if(transmitBuffer.size() > 0){
+                  synchronized (transmitBuffer){
+                     for(Byte b : transmitBuffer) {
+                        this.out.write(b);
+                     }
+                     transmitBuffer.clear();
+                  }
+               }
             }
          }
-         catch ( IOException e )
-         {
+         catch ( IOException e ) {
             e.printStackTrace();
          }
       }
 
       public void write(InstructorCommand command){
-          byte[] value = command.flatten();
-          Collections.addAll(transmitBuffer, InstructorCommand.transmitPrelude());
-          for(byte b : value){
-              transmitBuffer.add(b);
-          }
-          Collections.addAll(transmitBuffer, InstructorCommand.transmitPostlude(command));
+         byte[] value = command.flatten();
+         synchronized (transmitBuffer) {
+            Collections.addAll(transmitBuffer, InstructorCommand.transmitPrelude());
+            for (byte b : value) {
+               transmitBuffer.add(b);
+            }
+            Collections.addAll(transmitBuffer, InstructorCommand.transmitPostlude(command));
+         }
       }
    }
 
